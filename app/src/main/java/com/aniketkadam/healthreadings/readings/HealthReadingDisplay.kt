456c0a94f1e636c2542979136c2729a16848359a
Aniket Kadam <anikadamg@gmail.com>
2021-04-08 18:38:35 +0530

    Allowing editing the readings, nearly. Also added a placeholder time.

--- app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReadingDisplay.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReadingDisplay.kt
@@ -1,31 +1,45 @@
 package com.aniketkadam.healthreadings.readings
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun HealthReadingDisplay(reading: HealthReading) {
-    Card(Modifier.width(84.dp)) {
-        Row(modifier = Modifier.padding(4.dp)){
-            Column(Modifier.padding(end = 4.dp)) {
-                Text("🇴 ")
-                Text("💓 ")
-                Text("🌡️ ")
-                Text("😮‍💨")
+fun HealthReadingDisplay(reading: HealthReading, onClick: (HealthReading) -> Unit) {
+    Card(
+        Modifier
+            .width(84.dp)
+            .clickable(onClick = { onClick(reading) }, onClickLabel = "Edit", role = Role.Button)
+    ) {
+        Column(Modifier.padding(4.dp)) {
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp)
+            ) {
+                Text("3:30pm", color = Color.DarkGray)
             }
-            Column {
-                Text("${reading.oxygenation ?: " - "}")
-                Text("${reading.pulse ?: " - "}")
-                Text("${reading.temperature ?: " - "}")
-                Text("${reading.respiratoryRate ?: " - "}")
+            Row {
+                Column(Modifier.padding(end = 4.dp)) {
+                    Text("🇴 ")
+                    Text("💓 ")
+                    Text("🌡️ ")
+                    Text("😮‍💨")
+                }
+                Column {
+                    Text("${reading.oxygenation ?: " - "}")
+                    Text("${reading.pulse ?: " - "}")
+                    Text("${reading.temperature ?: " - "}")
+                    Text("${reading.respiratoryRate ?: " - "}")
+                }
             }
         }
     }
@@ -33,20 +47,20 @@ fun HealthReadingDisplay(reading: HealthReading) {
 
 @Preview
 @Composable
-fun PreviewHealthReadingWithAllValues(){
+fun PreviewHealthReadingWithAllValues() {
     HealthReadingDisplay(reading = HealthReading().apply {
         oxygenation = 99
         pulse = 80
         temperature = 97.5f
         respiratoryRate = 18
-    })
+    }) {}
 }
 
 @Preview
 @Composable
-fun PreviewHealthReadingWithSomeValuesMissing(){
+fun PreviewHealthReadingWithSomeValuesMissing() {
     HealthReadingDisplay(reading = HealthReading().apply {
         oxygenation = 99
         pulse = 80
-    })
+    }) {}
 }
