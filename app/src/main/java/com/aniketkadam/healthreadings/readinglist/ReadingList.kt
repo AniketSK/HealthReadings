ea1345a49e2cd69756fe08c0eb261f778ebde461
Aniket Kadam <anikadamg@gmail.com>
2021-04-08 18:48:44 +0530

    Using LazyVerticalGrid instead

--- app/src/main/java/com/aniketkadam/healthreadings/readinglist/ReadingList.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/readinglist/ReadingList.kt
@@ -1,7 +1,9 @@
 package com.aniketkadam.healthreadings.readinglist
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -10,16 +12,27 @@ import com.aniketkadam.healthreadings.readings.HealthReading
 import com.aniketkadam.healthreadings.readings.HealthReadingDisplay
 import kotlin.random.Random
 
+@ExperimentalFoundationApi
 @Composable
 fun ReadingList(readings: List<HealthReading>, onItemClicked: (HealthReading) -> Unit) {
 
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(84.dp),
+        contentPadding = PaddingValues(4.dp)
+    ) {
         items(readings) {
             HealthReadingDisplay(it, onItemClicked)
         }
     }
+
+//    LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+//        items(readings) {
+//            HealthReadingDisplay(it, onItemClicked)
+//        }
+//    }
 }
 
+@ExperimentalFoundationApi
 @Preview
 @Composable
 fun PreviewReadingsList() {
