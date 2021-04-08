4c895e538b414ede98336420e29b6507d8864375
Aniket Kadam <anikadamg@gmail.com>
2021-04-08 18:49:07 +0530

    Needed to add experimental tags for lazy vertical grid

--- app/src/main/java/com/aniketkadam/healthreadings/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +23,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
