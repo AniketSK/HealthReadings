bf92b12c5e06576d7c535d2c28b8f56bc4af7253
Aniket Kadam <anikadamg@gmail.com>
2021-04-09 09:55:24 +0530

    Bumped minsdk to support localdate conversions Added time display and clickability to the time picker.

--- app/src/main/java/com/aniketkadam/healthreadings/composer/ReadingComposer.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/composer/ReadingComposer.kt
@@ -1,20 +1,23 @@
 package com.aniketkadam.healthreadings.composer
 
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aniketkadam.healthreadings.readings.HealthReading
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.datetimepicker
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.util.*
 
 @Composable
 fun ReadingComposer(
@@ -40,6 +43,16 @@ fun ReadingComposer(
         )
     }
 
+    var savedDate by rememberSaveable {
+        mutableStateOf(healthReading?.date ?: Date())
+    }
+
+    val dialog = remember { MaterialDialog() }
+    dialog.build {
+        datetimepicker {
+            savedDate = Date.from(it.atZone(ZoneId.systemDefault()).toInstant())
+        }
+    }
     Box(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -70,6 +83,16 @@ fun ReadingComposer(
                 onValueChange = { respiratoryRate = it },
                 label = { Text("Respiratory Rate") },
                 leadingIcon = { Text("😮‍💨") })
+
+            Text(
+                "At ${
+                    SimpleDateFormat(
+                        "hh:mm E dd/MMM/yy",
+                        Locale.getDefault()
+                    ).format(savedDate)
+                }", Modifier.clickable {
+                    dialog.show()
+                })
 
             Button(
                 {
