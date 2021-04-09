ed2d5d5ef1e70eef8c1c7c4b701d1e3ac9feb2cf
Aniket Kadam <anikadamg@gmail.com>
2021-04-09 08:20:55 +0530

    Added serializers for HealthReading
    So it can be passed around to the edit view

--- app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReading.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReading.kt
@@ -1,19 +1,26 @@
 package com.aniketkadam.healthreadings.readings
 
+import com.aniketkadam.healthreadings.serializer.DateAsLongSerializer
+import com.aniketkadam.healthreadings.serializer.ObjectIdSerializer
 import io.realm.RealmModel
 import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
+import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
 import java.util.*
 
 @RealmClass
+@Serializable
 open class HealthReading : RealmModel {
     @PrimaryKey
+    @Serializable(with = ObjectIdSerializer::class)
     var _id: ObjectId = ObjectId.get()
     var temperature: Float? = null
-    var respiratoryRate : Int? = null
-    var oxygenation : Int? = null
+    var respiratoryRate: Int? = null
+    var oxygenation: Int? = null
     var pulse: Int? = null
+
+    @Serializable(with = DateAsLongSerializer::class)
     var date: Date = Date()
     var userId: String = ""
 }
