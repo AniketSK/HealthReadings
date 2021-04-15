44edd413db06c23b904f0fadf730a784a7f61b2b
Aniket Kadam <anikadamg@gmail.com>
2021-04-15 18:01:21 +0530

    Moved a check that should happen in data classes into the base class

--- app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReadingDisplayData.kt
+++ app/src/main/java/com/aniketkadam/healthreadings/readings/HealthReadingDisplayData.kt
@@ -1,12 +1,14 @@
 package com.aniketkadam.healthreadings.readings
 
+import com.aniketkadam.healthreadings.readinglist.BaseDisplayClass
 import java.text.SimpleDateFormat
 import java.util.*
 
 data class HealthReadingDisplayData(
     val healthReading: HealthReading,
     val nullNumberRepresentation: String
-) {
+) : BaseDisplayClass() {
+
     val displayDate: String =
         SimpleDateFormat("hh:mm\ndd MMM", Locale.getDefault()).format(healthReading.date)
     val date = healthReading.date
@@ -15,10 +17,6 @@ data class HealthReadingDisplayData(
     val oxygenation: String = numberFormatter(healthReading.oxygenation)
     val pulse: String = numberFormatter(healthReading.pulse)
     val userId: String = healthReading.userId
-
-    init {
-        check(Thread.currentThread().name != "main") { "Don't do this conversion on the main thread" }
-    }
 
     private fun numberFormatter(n: Number?): String {
         return if (n == null) {
